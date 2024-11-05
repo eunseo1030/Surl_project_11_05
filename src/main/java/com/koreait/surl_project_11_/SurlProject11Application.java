aa56f8b3f79dda09a6079dfa3d0d40d6dd8b4ae1
eunseo1030 <eunseo20001030@gmail.com>
2024-11-05 14:35:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/koreait/surl_project_11_/SurlProject11Application.java
@@ -0,0 +1,15 @@
+package com.koreait.surl_project_11_;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurlProject11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurlProject11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/surl_project_11_/SurlProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.surl_project_11_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurlProject11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
